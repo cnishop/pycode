4a49484cde8fb1a9e7668063ecb3638dfca0cced
cnishop <cnishop@126.com>
2018-09-19 17:29:41 +0800

    1
    1

--- helloword.py
+++ helloword.py
@@ -2,8 +2,11 @@ print('hello world')
 print("what is your name:")
 name= input()
 print("it is good for you:"+name)
-print("your name length is"+ str(len(name)))
+print("your name length is:"+ str(len(name)))
 
 dollar = 5;
 CNY= dollar *6.7
 print("{} dollar is {} RMB".format(dollar,CNY))
+
+
+print('The quick brown fox', 'jumps over', 'the lazy dog')
