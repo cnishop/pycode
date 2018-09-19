be8dfac15221d9b3406cd02da14f1b81fcc42de0
cnishop <cnishop@126.com>
2018-09-19 23:24:36 +0800

    1
    1

--- helloword.py
+++ helloword.py
@@ -10,3 +10,11 @@ print("{} dollar is {} RMB".format(dollar,CNY))
 
 
 print('The quick brown fox', 'jumps over', 'the lazy dog')
+
+
+age = 20
+if age >= 18:
+    print('your age is', age)
+    print('adult')
+
+
