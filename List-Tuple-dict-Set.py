eb320b000f2fdab68bd5b952a5464dc8afbbb64f
cnishop <cnishop@126.com>
2018-09-20 21:25:40 +0800

    1
    1

---
+++ List-Tuple-dict-Set.py
@@ -0,0 +1,13 @@
+names = ['Michael', 'Bob', 'Tracy']
+names.append('aaaa')
+print(names)
+
+classmates = ('Michael', 'Bob', 'Tracy')
+print(classmates)
+
+d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
+print(d)
+
+s = set([1, 1, 2, 2, 3, 3])
+s.add(4)
+print(s)
