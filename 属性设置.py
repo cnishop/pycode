093471219f381a804f057c4daa6258717f37cec1
cnishop <cnishop@126.com>
2018-11-07 17:31:36 +0800

    1
    1

--- 属性设置.py
+++ 属性设置.py
@@ -35,3 +35,22 @@ class Duck3(): #用装饰器实现  _name  aa._Duck3__name
     def name(self,input_name):
         print('inside the setter')
         self.__name = input_name
+
+class Student(object):
+    def __init__(self,name,score):
+        self.__name=name
+        self.__score=score
+    def print_socre(self):
+        print('%s:%s' % (self.__name,self.__score))
+    def get_name(self):
+        return self.__name
+    def get_score(self):
+        return self.__score
+
+bart = Student('Bart Simpson', 59)
+bart.get_name()
+bart.print_socre()
+bart.__name="ggg"    #注意不能这样写，相当月创建了一个新的变量
+bart.print_socre()
+print(bart._Student__name)
+print(bart.__name)
